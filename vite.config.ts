a7c0d15538678285ef860e2a588e5dd27ff49974
Thayane Batista <thayane.batista.ec@gmail.com>
2025-06-06 10:09:14 -0400

    [NEW]: Add GitHub Actions workflow for deploying to GitHub Pages and update Vite config for production base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/chuck-norris/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
